56c9fe852a8ec092452d7eeac0bc998d5e66f9fc
siradam7th <siradam7th@users.noreply.github.com>
2021-10-29 01:57:25 +0100

    add window creation and handling for the sandbox

--- sandbox/Application.cpp
+++ sandbox/Application.cpp
@@ -1,10 +1,68 @@
 #include "Application.h"
 using namespace Sandbox;
 
+// used for registering the systems
+#include "engine/runtime/systems/core/CoreSystem.h"
+// used to create and handle a window and its events
+#include "engine/runtime/systems/window/WindowSystem.h"
 
 bool Application::Init()
 {
     // TODO: implement application initialization here, anything that is specific to an app
+    auto window_system = deadrop::systems::Register<deadrop::systems::WindowSystem>();
+
+    // prepare a description for the window that we will create
+    deadrop::systems::WindowDesc window_desc{};
+    // this is the name used by Windows to refer to this window
+    window_desc.name = "DeadropSandboxGame";
+    // this is the actual title that you see in the window
+    window_desc.title = "Deadrop Sandbox Game";
+    window_desc.width = 1280;
+    window_desc.height = 720;
+
+    // initialize the window with the description we filled previously
+    bool window_initialized = window_system->Init(window_desc);
+    if (!window_initialized)
+    {
+        // error, failed to initialize the window
+        return false;
+    }
+
+    // show the actual windows
+    window_system->Show();
+
+    // prepare the keyboard callback handlers for the events the window can fire
+    auto WindowEvenHandler_OnKeyDown = [](u8 key) {
+        // a key down event was fired
+        // the first argument says which key was down
+    };
+
+    auto WindowEvenHandler_OnKeyUp = [](u8 key) {
+        // a key up event was fired
+        // the first argument says which key was up
+    };
+
+    auto WindowEvenHandler_OnLostKeyboardFocus = [](u64 window_handle)
+    {
+        // the window just lost keyboard focus
+        // the first argument is a handle to the window that lost the keyboard focus
+        // NOTE: treat this event like an OnKeyUp event for all the down keys (invalidate their pressed state)
+        // since the window no longer has keyboard focus
+    };
+
+    // register the event handlers that we created previously
+    // so they can get called by the window when event happen
+    window_system->SetKeyboardCallbacks(WindowEvenHandler_OnKeyDown,
+        WindowEvenHandler_OnKeyUp, WindowEvenHandler_OnLostKeyboardFocus);
+
+    // update the window for it stay responsive
+    // when the user closes the windows, Update() will return true,
+    // which causes this loop to stop looping
+    while (!window_system->Update())
+    {
+        // TODO: replace this loop with an actual game loop
+        continue;
+    }
 
     // return success
     return true;
